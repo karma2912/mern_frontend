165a207aadddaad5a66f268c22631c9a898b1611
karma2912 <rajakyash23@gmail.com>
2024-08-07 14:10:05 +0530

    fronttt commit

--- src/components/Test/Second.jsx
+++ src/components/Test/Second.jsx
@@ -18,7 +18,6 @@ const Second = () => {
     <TimeSubmit/>
     <Questions question={question} answer_a={answer_a} answer_b={answer_b} answer_c={answer_c} answer_d={answer_d} correctAnswer={correctAnswer}/>
     <Progress/>
-    <button onClick={stopFunction}>Stoppp</button>
     </div>
     </>
   )
